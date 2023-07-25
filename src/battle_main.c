e4e1e935626a01f260c99a9ee14c4c41bbb3dfcf
Shiny Master of all <101048219+Shiny-Miner@users.noreply.github.com>
2023-07-25 18:23:10 +0530

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -3176,7 +3176,11 @@ static void HandleTurnActionSelectionState(void)
                     }
                     break;
                 case B_ACTION_USE_ITEM:
+#ifdef TX_DEBUGGING
+                    if (FlagGet(FLAG_SYS_NO_BAG_USE) || gBattleTypeFlags & (BATTLE_TYPE_LINK | BATTLE_TYPE_BATTLE_TOWER | BATTLE_TYPE_EREADER_TRAINER))
+                    #else
                     if (gBattleTypeFlags & (BATTLE_TYPE_LINK | BATTLE_TYPE_BATTLE_TOWER | BATTLE_TYPE_EREADER_TRAINER))
+                    #endif
                     {
                         gSelectionBattleScripts[gActiveBattler] = BattleScript_ActionSelectionItemsCantBeUsed;
                         gBattleCommunication[gActiveBattler] = STATE_SELECTION_SCRIPT;
